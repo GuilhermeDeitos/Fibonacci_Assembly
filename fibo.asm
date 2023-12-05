8bd15f36b6b0c8f3b2ec47ebc535718d0d9295d8
Guilherme Deitos <guilherme.cascavel@gmail.com>
2023-12-05 20:47:04 -0300

    Erros corrigidos, apenas enter na nomeação

--- fibo.asm
+++ fibo.asm
@@ -1,5 +1,5 @@
 ;Trabalho Fibonacci Guilherme Deitos e Gabriel Yudi
-%define maxChars 10
+%define maxChars 3
 
 section .data
     strPrint : db "Digite o numero para calcularmos o fibonacci: "
@@ -11,7 +11,8 @@ section .data
     strLF  : db 10 ; quebra de linha
     strLFL : equ 1
 
-    strNomeArq: db "fib(n).bin"
+    strNomeArq: db "fib("
+    strDotBin: db ").bin"
 section .bss
     strInput : resb maxChars
     strInputL : equ $ - strInput
@@ -20,25 +21,16 @@ section .bss
     n2: resd 1
     numFibo : resb 2
     buffer  : resb 32
+    newFileName : resb 32  ; Novo nome do arquivo
+    fib_arq : resq 1
 
 
-    resultFibonacci : resd 8
-    resultFibonacciL : equ $ - resultFibonacci
 
 section .text
     global _start
 
 
 _start:
-
-openArq:
-    ; Abrir arquivo
-    mov rax, 2
-    lea rdi, [strNomeArq]
-    mov rsi, 102o  ; createOpenR
-    mov rdx, 644o  ; userPermissions
-    syscall
-    mov [fileHandle], rax
 
 write:
     mov rax, 1
@@ -56,23 +48,74 @@ read:
     syscall
 
 
+
+verificaTamanho:
+    cmp byte [strInput], 0xa
+    je fim
+
+    cmp byte [strInput + 1], 0xa
+    je nome1Digito
+
+    cmp byte [strInput + 2], 0xa
+    je nome2Digitos
+
+    jmp writeArqError
+
+nome1Digito:
+    mov al, [strInput]
+    mov rbx, [strNomeArq]
+    mov [newFileName], rbx
+    mov [newFileName + 4], al
+    mov rbx, [strDotBin]
+    mov [newFileName + 5], rbx
+    mov bl, [strDotBin + 4]
+    mov [newFileName + 9], bl  
+
+nome2Digitos:
+    mov cl, [strInput + 1]
+        mov al, [strInput]
+        mov rbx, [strNomeArq]
+        mov [newFileName], rbx
+        mov [newFileName + 4], al 
+        mov [newFileName + 5], cl 
+        mov rbx, [strDotBin] 
+        mov [newFileName + 6], rbx
+        mov bl, [strDotBin + 4]
+        mov [newFileName + 10], bl
+
+
+    ; Abrir arquivo
+    mov rax, 2
+    lea rdi, [newFileName]
+    mov rsi, 102o  ; createOpenR
+    mov rdx, 644o  ; userPermissions
+    syscall
+    mov [fileHandle], rax
+
+
 tratandoValores:
     mov r9b, byte [strInput]
     sub r9b, '0' ;transforma o valor em inteiro
     mov r10b, byte [strInput+1]
 
-    cmp r10d, 0xa; Verifica se é enter
-    je verificaLimites ;Caso tenha 1 digito, já passa para o verifica limites, caso tenha 2, é tratado isso com o r10d
+    cmp r10b, 0xa; Verifica se é enter
+    je auxiliar ;Caso tenha 1 digito, já passa para o verifica limites, caso tenha 2, é tratado isso com o r10d
 
 
-    mov r10b, byte[strInput]
-    sub r10d, '0' ;transforma o valor em inteiro
+    mov r10b, byte [strInput+1] 
+    sub r10b, '0'                ; Transforma o valor em inteiro
 
     mov rax, 0xa
-    mul r9d
-    add eax, r10d 
+    mul r9b
+    add eax, r10d
+    jmp verificaLimites
+
+auxiliar:
+    mov eax, r9d
+
 
 verificaLimites:
+
     cmp ax, 99
     jle calcFibo
 
@@ -83,25 +126,29 @@ verificaLimites:
 
 
 calcFibo:
-    cmp byte [strInput], 1
+    mov byte[n1], 0
+    cmp ax, 0
     je writeArq
 
-    cmp byte [strInput], 2
-    je writeArq
-
-    mov byte [n1], 1; f(n-2)
+    mov byte [n1], 1; f(n-1)
     mov byte [n2], 1; f(n-2)
-    mov r13d, 3
+
+    cmp ax, 1
+    je writeArq
+
+    cmp ax, 2
+    je writeArq
+
 
 forFibo:
     mov r11d, [n1]
-    mov r12d, [n1]
+    mov r12d, [n2]
     add r12d, [n1]
     mov [n1], r12d
     mov [n2], r11d
-    cmp r13d, 0
+    cmp eax, 3
     je writeArq
-    inc r13d
+    dec eax
     jnz forFibo
 
 limpabuffer:
